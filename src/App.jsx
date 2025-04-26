b8a5299c1e95cda14740427b8e3352113b5ab9e3
Ayush-Khan <ayushkhana230@gmail.com>
2025-04-26 03:28:26 -0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Index from "./router/Index";
 function App() {
   // const API_URL = import.meta.env.VITE_API_URL;
   // const API_URL = import.meta.env.API_URL;
-  // console.log("this is API_URL", API_URL);
+  // consol.log("this is API_URL", API_URL);
+
   return (
     <div className="overflow-x-hidden">
       <Index />
