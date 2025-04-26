276421317b4733f7ce7f58edd7d3618ce5821953
Ayush-Khan <ayushkhana230@gmail.com>
2025-04-25 22:10:50 -0700

    check commit to my accound

--- srcForTest/componants/common/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-// LoadingSpinner.js
-
-import styles from "../../CSS/LoadingSpinner.module.css";
-
-const LoadingSpinner = () => {
-  return <div className={styles.spinner}></div>;
-};
-
-export default LoadingSpinner;
